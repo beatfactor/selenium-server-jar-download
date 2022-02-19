675f1a3a910f53ba447cbfa793f0c5b398199727
Andrei Rusu <andrei.rusu@beatfactor.net>
2022-02-19 20:18:18 +0100

    updated v3

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const path = require('path');
 const packageInfo = require('./package.json');
 const {version, selenium} = packageInfo;
+const {versionNumber = version} = selenium;
 
 module.exports = {
-  path: path.join(__dirname, 'lib', `${selenium.filename}-${version}.jar`)
+  path: path.join(__dirname, 'lib', `${selenium.filename}-${versionNumber}.jar`)
 }
--- test/testDownload.js
+++ test/testDownload.js
@@ -5,21 +5,22 @@ const packageInfo = require('../package.json');
 
 describe('test download', function() {
   const {version, selenium} = packageInfo;
+  const {versionNumber = version} = selenium;
 
   it('tests the config', function() {
     const config = require('../install.conf.js');
-    assert.strictEqual(config.downloadUrl, `https://github.com/SeleniumHQ/selenium/releases/download/selenium-${selenium.folder}/${selenium.filename}-${version}.jar`);
+    assert.strictEqual(config.downloadUrl, `https://github.com/SeleniumHQ/selenium/releases/download/selenium-${selenium.folder}/${selenium.filename}-${versionNumber}.jar`);
   })
 
   it('tests the download', async function() {
     const download = require('../download.js');
     await download();
 
-    assert.strictEqual(fs.existsSync(path.join(__dirname, `../lib/${selenium.filename}-${version}.jar`)), true);
+    assert.strictEqual(fs.existsSync(path.join(__dirname, `../lib/${selenium.filename}-${versionNumber}.jar`)), true);
   });
 
   it('tests the require path', function() {
     const {path: filePath} = require('../index.js');
-    assert.strictEqual(filePath, path.join(__dirname, `../lib/${selenium.filename}-${version}.jar`));
+    assert.strictEqual(filePath, path.join(__dirname, `../lib/${selenium.filename}-${versionNumber}.jar`));
   });
 })
